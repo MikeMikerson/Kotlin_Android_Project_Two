6ca0b2cffe36df8c648f171791c30a659f854d6a
Mike Farrelly <simplicitydevs@gmail.com>
2021-01-11 06:45:36 +0900

    Added APOD using retrofit

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,14 +3,31 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.BuildConfig
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.api.NasaAPI
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    private val _imgUrl = MutableLiveData<String>()
-    val imgUrl: LiveData<String>
-        get() = _imgUrl
+    private val _apod = MutableLiveData<PictureOfDay>()
+    val apod: LiveData<PictureOfDay>
+        get() = _apod
 
     init {
-        // TODO: Change value into something meaningful
-        _imgUrl.value = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg"
+        retrieveImageOfTheDay()
+    }
+
+    private fun retrieveImageOfTheDay() {
+        viewModelScope.launch {
+            try {
+                _apod.value = NasaAPI.retrofitService.getImageOfTheDay(BuildConfig.API_KEY)
+            } catch (e: Exception) {
+                _apod.value = PictureOfDay(
+                    "",
+                    "",
+                    "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg")
+            }
+        }
     }
 }
